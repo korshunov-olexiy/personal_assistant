8639da23cbcca0bc2d032d1c614a529a76460d0b
Olexii Korshunov <korshunov.olexiy@gmail.com>
2021-12-10 23:05:58 +0200

    update CommandHandler class, add_record, holidays_period methods

--- personal_manager.py
+++ personal_manager.py
@@ -1,18 +1,22 @@
 import pickle
 import re
 from collections import UserDict
-from datetime import datetime
+from datetime import datetime, timedelta
 from difflib import get_close_matches
 from pathlib import Path
 from typing import List, Optional, Dict, Tuple
 
-from pick import pick
+#from pick import pick
 
 
 def standard_input():
-    yield "hely"
-    yield "add"
-    yield "help"
+    yield "add_user"
+    yield "Vasya"
+    yield "165-34-54-221,123-34-567-01,456-12-345-67"
+    yield "09.01.1990"
+    yield "Lvivska st., Lviv, 32/56; Kyivska st., Kyiv, 56/42"
+    yield "holidays_period"
+    yield "30"
     yield "exit"
 
 
@@ -71,6 +75,21 @@ class Phone(Field):
         return f"Phone: {self.value}"
 
 
+class Address(Field):
+    """Address class for storage address's field"""
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+    def __str__(self):
+        return f"Address: {self.value}"
+
+
 class Birthday(Field):
     """Birthday class for storage birthday"s field"""
 
@@ -90,7 +109,7 @@ class Record:
     """Record class responsible for the logic of adding/removing/editing fields
     Only one name but many phone numbers"""
 
-    def __init__(self, name: str, phone: List[str] = None, birthday: Optional[str] = None) -> None:
+    def __init__(self, name: str, phone: List[str] = None, birthday: Optional[str] = None, address: Optional[str] = None) -> None:
         self.phone = []
         for one_phone in phone:
             try:
@@ -98,6 +117,9 @@ class Record:
             except InvalidPhoneNumber:
                 print(f"The phone number {one_phone} is invalid")
         self.name = Name(name)
+        self.address = []
+        for one_address in address:
+            self.address.append(Address(one_address))
         if birthday:
             self.birthday = Birthday(birthday)
 
@@ -146,7 +168,9 @@ class Record:
     def __str__(self):
         result = f"Record of {self.name.value}"
         if self.phone:
-            result += f", phones: {[one_phone.value for one_phone in self.phone]}"
+            result += f", phones: {','.join([one_phone.value for one_phone in self.phone])}"
+        if self.address:
+            result += f", address: {'; '.join([one_address.value for one_address in self.address])}"
         if self.birthday.value:
             result += f", birthday: {self.birthday.value}"
             result += f", to birthday: {self.days_to_birthday()}"
@@ -156,9 +180,39 @@ class Record:
 class AddressBook(UserDict):
     """Add new instance of Record class in AddressBook"""
 
-    def add_record(self, name: str, phones: List[str] = None, birthday: Optional[str] = None) -> None:
-        new_record = Record(name, phones, birthday)
+    def __get_params(self, params: Dict[str,str]) -> Dict[str,str]:
+        msg = "Please enter the "
+        params_keys = list(params.keys())
+        for index in range(len(params)):
+            obj_name = params_keys[index]
+            if obj_name == "phone":
+                params[obj_name] = input(f"{msg}{obj_name}. Separator character for phone number is ',': ").split(",")
+            elif obj_name == "address":
+                params[obj_name] = input(f"{msg}{obj_name}. Separator character for address is ';': ").split(";")
+            else:
+                params[obj_name] = input(f"{msg}{obj_name}: ")
+        return params.values()
+
+    def add_record(self) -> None:
+        new_record = Record(*self.__get_params({"name": "", "phone": "", "birthday": "", "address": ""}))
         self.data[new_record.name.value] = new_record
+
+    def holidays_period(self) -> None:
+        period = int(''.join(self.__get_params({"period": ""})))
+        result = {}
+        birthdays = {rec.birthday.value: name for name, rec in self.data.items()}
+        dates = {date[:5]: date[5:] for date in birthdays}
+        today = datetime.today()
+        print(f"Search results for birthdays for a period of {period} days:")
+        for one_day in range(period+1):
+            today_period = (today + timedelta(days=one_day)).strftime("%d.%m")
+            if today_period in dates:
+                date = f"{today_period}{dates[today_period]}"
+                result.update({birthdays[date]: date})
+        if not len(result):
+            print("No contacts found with birthdays for the specified period.")
+            return None
+        print("\n".join([f"{key} - {val}" for key,val in result.items()]))
 
     def find_record(self, value: str) -> Optional[Record]:
         return self.data.get(value.capitalize())
@@ -203,91 +257,65 @@ class AddressBook(UserDict):
             print(f"An error occurred while opening the file \"{filename}\"")
 
 
-class CommandHandler(UserDict):
+class CommandHandler:
 
     def __init__(self) -> None:
-        self.data = {}
+        '''self.data = {}'''
 
-    def parse_msg(self, len_of_list: int, input_msg: str) -> str:
-        msg = re.sub(r" +", " ", input_msg).split(" ", maxsplit=len_of_list)
-        return ' '.join(msg[:len_of_list]), ' '.join(msg[len_of_list:])
-
-    def __get_standard_command(self, input_msg: str, list_of_commands: Dict[str, str]) -> Optional[Tuple[str]]:
-        for key, commands in list_of_commands.items():
-            for cmd in commands:
-                raw_cmd, raw_msg = self.parse_msg(len(cmd.split()), input_msg)
-                cmd = ''.join(list(filter(lambda cmd: raw_cmd == cmd, commands)))
-                if cmd:
-                    self.data.update({key: {cmd: raw_msg}})
-
-    def __get_variants_commands(self, input_msg: str, list_of_commands: Dict[str, str]) -> None:
-        for key, commands in list_of_commands.items():
-            for cmd in commands:
-                raw_cmd, raw_msg = self.parse_msg(len(cmd.split()), input_msg)
-                match = ''.join(get_close_matches(raw_cmd, [cmd]))
-                if match:
-                    self.data.update({key: {match: raw_msg}})
-        print(self.data)
-
-    def get_input_msg(self, input_msg: str) -> Optional[int]:
-        self.__get_standard_command(input_msg, list_of_commands)
-        print(self.data)
-        if self.data:
-            if self.data.get(cmd_type.EXIT):
-                # if found exit command
-                return None
-            else:
-                # if found action command
-                commands_func[self.data](raw_msg)
-            asdf
-        elif not self.data:
-            self.__get_variants_commands(input_msg, list_of_commands)
-            """если """
-        for key, commands in list_of_commands.items():
-            for cmd in commands:
-                raw_cmd, raw_msg = self.parse_msg(len(cmd.split()), input_msg)
-                res_cmd, res_msg = self.__get_standard_command(input_msg, list_of_commands)
-                if not res_cmd:
-                    match = ''.join(get_close_matches(raw_cmd, [cmd]))
-                if key == cmd_type.EXIT and match:
-                    return None
-                elif key == cmd_type.ACTION and match:
-                    self.data.update({match: raw_msg})
-        if not self.data:
-            print("Sorry, I could not recognize the entered command!")
-            return 1
-        else:
-            if len(self.data) > 1:
-                option, _ = pick(self.data, TITLE, indicator="=>")
-            else:
-                option = self.data.keys()[0]
-            commands_func[option](raw_msg)
+    def __call__(self, command: str) -> bool:
+        cmd = command
+        if cmd in exit_commands:
+            return False
+        elif cmd in action_commands:
+            commands_func[cmd]()
+            return True
 
 
+def  cmd_save_note():
+    ''''''
+def  cmd_edit_note():
+    ''''''
+def  cmd_del_note():
+    ''''''
+def  cmd_sort_note():
+    ''''''
+def  cmd_find_note():
+    ''''''
+def  cmd_add_tag():
+    ''''''
+def  cmd_sort_files():
+    ''''''
+def  cmd_find_contact():
+    ''''''
+def  cmd_edit_contact():
+    ''''''
+def  cmd_del_contact():
+    ''''''
+book = AddressBook()
 TITLE = "We have chosen several options from the command you provided.\nPlease choose the one that you need."
-action_commands = ["help", "hello", "add ", "change", "phone", "show all"]
-exit_commands = ["good bye", "close", "exit"]
+action_commands = ["add_user", "holidays_period", "save_note ", "edit_note", "del_note", "sort_note", "find_note", "add_tag", "sort_files", "find_contact", "edit_contact", "del_contact"]
+exit_commands = ["bye", "close", "exit"]
+functions_list = [book.add_record, book.holidays_period, cmd_save_note, cmd_edit_note, cmd_del_note, cmd_sort_note, cmd_find_note, cmd_add_tag, cmd_sort_files, cmd_find_contact, cmd_edit_contact, cmd_del_contact]
+commands_func = {cmd: func for cmd, func in zip(action_commands, functions_list)}
 cmd_type = TypeOfCommand()
 list_of_commands = {cmd_type.EXIT: exit_commands, cmd_type.ACTION: action_commands}
-
-#functions_list = [cmd_help, cmd_hello, cmd_add, cmd_change, cmd_phone, cmd_show_all]
-#functions_list = ['cmd_help', 'cmd_hello', 'cmd_add', 'cmd_change', 'cmd_phone', 'cmd_show_all']
-#commands_func = {cmd: func for cmd, func in zip(commands_list, functions_list)}
-
 
 if __name__ == "__main__":
     current_script_path = Path(__file__).absolute()
     file_bin_name = f"{current_script_path.stem}.bin"
-    book = AddressBook()
+    
     #data_file = current_script_path.parent.joinpath(file_bin_name)
     """get data file from current directory"""
     #book.load_data(data_file)
     cmd = CommandHandler()
+    #re.sub(r" +", " ", input("Hello, please enter the command: ")
     input_msg = input("Hello, please enter the command: ").lower().strip()
     # while cmd.get_input_msg(input_msg):
-    if cmd.get_input_msg(input_msg):
+    while cmd(input_msg):
         """run again"""
         input_msg = input("Please enter the command: ").lower().strip()
 
-    # book.save_data(data_file)
     print("Have a nice day... Good bye!")
+    for rec in book.iterator(2):
+        print(rec)
+    # book.save_data(data_file)
