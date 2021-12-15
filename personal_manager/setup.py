676011c5d423dab3e34a2dd44e532221a880d662
Bernatovych <leaderr99@gmail.com>
2021-12-15 15:53:09 +0200

    edit project

--- personal_manager/setup.py
+++ personal_manager/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='personal_manager',
-      version='0.5',
+      version='0.7',
       description='Maintaining a personal address book.',
       url='https://github.com/korshunov-olexiy/personal_assistant',
       author='Александр Бернатович, Angelika Kuzmina, Olexii Korshunov',
       author_email='leaderr99@gmail.com, angelikak199712@gmail.com, korshunov.olexiy@gmail.com',
       license='MIT',
       install_requires=['pick==1.2.0', 'wheel'],
-      packages=['personal_manager'],
-      #packages=find_namespace_packages()
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['personal-manager=personal_manager.personal_manager:main']}
       )
